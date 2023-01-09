deb6a07d4b9221f865869acc67ccb30493f198a5
takkeR-DEV <takkerwin@yandex.ru>
2023-01-09 04:12:18 +0300

    movies

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 
+import './App.css';
 import CardList from '../CardList/CardList';
+
 export default class App extends Component {
   render() {
     return (
