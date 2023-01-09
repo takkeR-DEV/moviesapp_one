8422ed496ef1f7e62454adf99ffd6df402e2c54e
takkeR-DEV <takkerwin@yandex.ru>
2023-01-09 04:03:00 +0300

    movies

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,7 +6,7 @@ export default class App extends Component {
     return (
       <div>
         <div className="">
-          <CardList />
+          <CardList /> ewgewg
         </div>
         <div>Hello</div>
       </div>
